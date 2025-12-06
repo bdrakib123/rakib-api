6b758b48ce82c13a586ba16836716e266212534e
Rakib <spyer.rakib@gmail.com>
2025-12-07 02:00:15 +0600

    Implement image serving APIs with Express

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+
+function sendImage(res, fileName) {
+  const imgPath = path.join(__dirname, fileName);
+
+  if (!fs.existsSync(imgPath)) {
+    return res.status(404).json({ error: `${fileName} not found on server` });
+  }
+
+  const img = fs.readFileSync(imgPath);
+  res.setHeader("Content-Type", "image/png");
+  res.send(img);
+}
+
+// kiss image API
+app.post("/api/kiss", (req, res) => {
+  sendImage(res, "kiss.png");
+});
+
+// pair image API
+app.post("/api/pair", (req, res) => {
+  sendImage(res, "pair.png");
+});
+
+// crush image API
+app.post("/api/crush", (req, res) => {
+  sendImage(res, "crush.png");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
